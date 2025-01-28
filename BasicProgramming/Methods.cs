0e6673fff78b83a2ba546db134d44757b68d34d7
Basava Duryodhana <duryodhanareddy2@gmail.com>
2025-01-28 14:24:16 +0530

    Adding methods calling.

---
+++ BasicProgramming/Methods.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getting_started_with_CSharp.BasicProgramming
+{
+    public class Methods
+    {
+        public int AddTwoNumber(int x, int y)
+        {
+            int z = x + y;
+            return z;
+        }
+
+        public class getHouseInfo
+        {
+            public string ownerName = "Duryodhana";
+            public string constructionYear = "2014";
+            public bool garage = true;
+            public static bool swimmingPool = true;
+            public static string builderCompany = "Aparna Builders Inc.";
+            public string houseType = "Villa";
+            public string colour = "Pale green";
+
+            public string houseDetails()
+            {
+                return "Owner Name: " + ownerName + "\n" +
+                       "Construction Year: " + constructionYear + "\n" +
+                       "Has Garage: " + garage + "\n" +
+                       "Has Swimming Pool: " + swimmingPool + "\n" +
+                       "Builder Company: " + builderCompany + "\n" +
+                       "House Type: " + houseType + "\n" +
+                       "House Color: " + colour;
+            }
+
+        }
+
+        public static void GetHousePrice()
+        {
+            Console.WriteLine("The house price is: 5000000");
+        }
+
+        public void DisplayTodaysDate()
+        {
+            Console.WriteLine("Today's date is: " + DateTime.Now);
+        }
+    }
+}
---
+++ BasicProgramming/Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace getting_started_with_CSharp.BasicProgramming
+{
+    public class Tests
+    {
+        Methods practice = new Methods();
+        [Test]
+        public void VerifyAddTwoNumber()
+        {
+            int result = practice.AddTwoNumber(10, 20);
+            Console.WriteLine("Result of AddTwoNumber(10, 20): " + result);
+        }
+
+        [Test]
+        public void GetHouseInfo()
+        {
+            Console.WriteLine("House Info:");
+            Methods.getHouseInfo house = new Methods.getHouseInfo();
+            string houseInfo = house.houseDetails();
+            Console.WriteLine(houseInfo);
+            Console.WriteLine("House Price:");
+            Methods.GetHousePrice();
+            Console.WriteLine("Today's Date:");
+            Methods.GetHousePrice();
+        }
+    }
+}
